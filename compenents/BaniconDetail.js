373d6149ba02311c69423e958215593b54e32eee
mylxn <489376680@qq.com>
2016-11-18 22:24:02 +0800

    add navigator of banicon

---
+++ compenents/BaniconDetail.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import React from 'react';
+
+import {
+  View,
+  StyleSheet,
+  Text
+} from 'react-native';
+
+class BaniconDetail extends React.Component{
+  render() {
+    return (
+      <Text>banicondetail</Text>
+    )
+  }
+}
+
+
+export { BaniconDetail as default}

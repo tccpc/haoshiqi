a556bf969d8ee7a69eee04530f6abc75da0ea7e1
tccpc <648593412@qq.com>
2016-11-19 16:47:16 +0800

    add storage

--- compenents/goodsLists.js
+++ compenents/goodsLists.js
@@ -1,6 +1,8 @@
 "use strict";
-import React, {Component} from 'react';
+import React, {Component ,AsyncStorage } from 'react';
 import GoodsDetail from './goodsDetail.js';
+import Login from './login.js';
+import Storage from 'react-native-storage';
 
 import {
   View,
@@ -12,6 +14,44 @@ import {
   TouchableHighlight,
   Del,
 } from 'react-native';
+
+var storage = new Storage({
+  // 最大容量，默认值1000条数据循环存储
+  size: 1000,
+
+  // 存储引擎：对于RN使用AsyncStorage，对于web使用window.localStorage
+  // 如果不指定则数据只会保存在内存中，重启后即丢失
+  storageBackend: AsyncStorage,
+
+  // 数据过期时间，默认一整天（1000 * 3600 * 24 毫秒），设为null则永不过期
+  defaultExpires: 1000 * 3600 * 24 * 7,
+
+  // 读写时在内存中缓存数据。默认启用。
+  enableCache: true,
+
+  // 如果storage中没有相应数据，或数据已过期，
+  // 则会调用相应的sync方法，无缝返回最新数据。
+  // sync方法的具体说明会在后文提到
+  // 你可以在构造函数这里就写好sync的方法
+  // 或是写到另一个文件里，这里require引入
+  // 或是在任何时候，直接对storage.sync进行赋值修改
+})
+
+// 使用key来保存数据。这些数据一般是全局独有的，常常需要调用的。
+  // 除非你手动移除，这些数据会被永久保存，而且默认不会过期。
+  storage.save({
+    key: 'user',  // 注意:请不要在key中使用_下划线符号!
+    rawData: {
+      num:'13804778325',
+      name:'崔哥哥',
+      pw:'12345678',
+      cart:[]
+    },
+
+    // 如果不指定过期时间，则会使用defaultExpires参数
+    // 如果设为null，则永不过期
+    expires: 1000 * 3600
+  });
 
 export default class GoodsLists extends Component {
   constructor(prop){
@@ -39,7 +79,54 @@ export default class GoodsLists extends Component {
     // alert(tit.name)
     this.props.navigator.push({name: 'GoodsDetail', component: GoodsDetail, passProps:goods,isTabBar:false})
   };
+  addCart(goods){
+    storage.load({
+      key: 'user',
 
+      // autoSync(默认为true)意味着在没有找到数据或数据过期时自动调用相应的sync方法
+      autoSync: true,
+
+      // syncInBackground(默认为true)意味着如果数据过期，
+      // 在调用sync方法的同时先返回已经过期的数据。
+      // 设置为false的话，则始终强制返回sync方法提供的最新数据(当然会需要更多等待时间)。
+      syncInBackground: false
+    }).then(ret => {
+      console.log(ret);
+      for (let i=0;i<ret.cart.length;i++){
+        if(ret.cart[i].skuId == goods.skuInfo.skuId){
+          ret.cart[i].count++;
+          storage.save({
+            key: 'user',  // 注意:请不要在key中使用_下划线符号!
+            rawData: {
+              num:'13804778325',
+              name:'崔哥哥',
+              pw:'12345678',
+              cart:ret.cart
+            }
+          });
+          return;
+        }
+      }
+      ret.cart.push({
+         skuId: goods.skuInfo.skuId,
+         count: 1,
+      })
+      storage.save({
+        key: 'user',  // 注意:请不要在key中使用_下划线符号!
+        rawData: {
+          num:'13804778325',
+          name:'崔哥哥',
+          pw:'12345678',
+          cart:ret.cart
+        }
+      });
+    }).catch(err => {
+      console.log('没找到');
+    })
+    // if(!user){
+    //   this.props.navigator.push({name: 'Login', component: Login,isTabBar:false})
+    // }
+  }
   onEndReached(){
 
   };
@@ -75,7 +162,12 @@ export default class GoodsLists extends Component {
             </View>
             <View style={indexStyle.cartBox}>
               <Text style={indexStyle.expired}>保质期至：{expired}</Text>
-              <Image source={{ uri:'ic_shopcart' }} style={indexStyle.cart}></Image>
+              <TouchableHighlight
+                onPress={() => this.addCart(good)}
+                underlayColor="rgba(255,255,255,0.3)"
+                >
+                <Image source={{ uri:'ic_shopcart' }} style={indexStyle.cart}></Image>
+              </TouchableHighlight>
             </View>
           </View>
         </View>
--- compenents/index.js
+++ compenents/index.js
@@ -49,7 +49,7 @@ export default class Index extends Component {
             </View>
           </View>
           <ScrollView>
-            <BannerList navigator={this.props.navigator} />
+            <BannerList />
             <FlashSale />
             <GoodsLists listUrl={GOOD_URL} navigator={this.props.navigator} />
           </ScrollView>

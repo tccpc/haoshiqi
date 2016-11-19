0545d6a3167e59534ad3601f784d6fdd5ef8ae50
tccpc <648593412@qq.com>
2016-11-19 16:51:53 +0800

    add storage

--- compenents/goodsLists.js
+++ compenents/goodsLists.js
@@ -37,21 +37,7 @@ var storage = new Storage({
   // 或是在任何时候，直接对storage.sync进行赋值修改
 })
 
-// 使用key来保存数据。这些数据一般是全局独有的，常常需要调用的。
-  // 除非你手动移除，这些数据会被永久保存，而且默认不会过期。
-  storage.save({
-    key: 'user',  // 注意:请不要在key中使用_下划线符号!
-    rawData: {
-      num:'13804778325',
-      name:'崔哥哥',
-      pw:'12345678',
-      cart:[]
-    },
 
-    // 如果不指定过期时间，则会使用defaultExpires参数
-    // 如果设为null，则永不过期
-    expires: 1000 * 3600
-  });
 
 export default class GoodsLists extends Component {
   constructor(prop){
@@ -121,7 +107,22 @@ export default class GoodsLists extends Component {
         }
       });
     }).catch(err => {
-      console.log('没找到');
+      console.log('用户登录');
+      // 使用key来保存数据。这些数据一般是全局独有的，常常需要调用的。
+        // 除非你手动移除，这些数据会被永久保存，而且默认不会过期。
+        storage.save({
+          key: 'user',  // 注意:请不要在key中使用_下划线符号!
+          rawData: {
+            num:'13804778325',
+            name:'崔哥哥',
+            pw:'12345678',
+            cart:[]
+          },
+
+          // 如果不指定过期时间，则会使用defaultExpires参数
+          // 如果设为null，则永不过期
+          expires: 1000 * 3600
+        });
     })
     // if(!user){
     //   this.props.navigator.push({name: 'Login', component: Login,isTabBar:false})
